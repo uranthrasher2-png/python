6c0f8c8b680d2a49f584a0ee06219295344eb623
Evgeniy <uranthrasher2@gmail.com>
2026-08-04 11:39:54 +0300

    added chars

--- 3) password generator.py
+++ 3) password generator.py
@@ -1,9 +1,10 @@
 import random 
 
-digits = 0123456789
-owercase_letters = abcdefghijklmnopqrstuvwxyz
-uppercase_letters = ABCDEFGHIJKLMNOPQRSTUVWXYZ
-punctuation = !#$%&*+-=?@^_
+digits = "0123456789"
+owercase_letters = "abcdefghijklmnopqrstuvwxyz"
+uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+punctuation = "!#$%&*+-=?@^_"
 
+chars = ''
 
 
