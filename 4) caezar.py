9ff96887b57ad6ea92a39b9893a39d0a6b08a2b8
Evgeniy <uranthrasher2@gmail.com>
2026-08-07 11:01:33 +0300

    final commit

--- 4) caezar.py
+++ 4) caezar.py
@@ -1,6 +1,8 @@
+purpose = input('Введите в поле - ш (шифровать) или д (дешифровать)').lower()
 while purpose not in ('ш', 'д'):
-    purpose = input('Необходимо ввести в поле - ш (шифровать) или д (дешифровать)').lower()
+    language = input('Необходимо ввести в поле - ш (шифровать) или д (дешифровать)').lower()
 
+language = input('Введите в поле - р (русский) или а (английский)').lower()
 while language not in ('р', 'а'):
     language = input('Необходимо ввести в поле - р (русский) или а (английский)').lower()
 
@@ -13,4 +15,28 @@ question = input('Введите текст для обработки: ')
 while not question.strip():  
     question = input('Вы не ввели текст! Попробуйте снова: ')
 
+def caesar(question, shift, purpose, language):
+    result = ''
+    if language == 'р':
+        alphabet_lower = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+        alphabet_upper = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+    else:
+        alphabet_lower = 'abcdefghijklmnopqrstuvwxyz'
+        alphabet_upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
+    shift_amount = shift if purpose == 'ш' else -shift
+
+    for char in question:
+        if char in alphabet_lower:
+            idx = alphabet_lower.index(char)
+            new_idx = (idx + shift_amount) % len(alphabet_lower)
+            result += alphabet_lower[new_idx]
+        elif char in alphabet_upper:
+            idx = alphabet_upper.index(char)
+            new_idx = (idx + shift_amount) % len(alphabet_upper)
+            result += alphabet_upper[new_idx]
+        else:
+            result += char
+    return result
+
+print(caesar(question, shift, purpose, language))
